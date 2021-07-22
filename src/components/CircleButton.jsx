70d97f459f2ac95b59ddd7c76d35a77778c2eaee
Kenta Shinozaki <kurometaro331@gmail.com>
2021-07-22 19:21:13 +0900

    App.jsxをcomponentsに切り分けた

---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { string } from 'prop-types';
+
+const CircleButton = (props)=> {
+  const { children } = props;
+  return(
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>{children}</Text>
+    </View>
+  );
+}
+
+CircleButton.propTypes = {
+  children: string.isRequired,
+}
+
+export default CircleButton;
+
+const styles = StyleSheet.create({
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#fff',
+    fontSize: 40,
+    lineHeight: 40,
+  },
+});
