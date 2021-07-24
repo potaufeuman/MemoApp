f02b5e86dd1c190d930c0e777f1c8f169d95bd05
Kenta Shinozaki <kurometaro331@gmail.com>
2021-07-25 08:04:01 +0900

    Login,Signup画面を作成

---
+++ src/screens/LogInScreen.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, } from 'react-native';
+import AppBar from '../components/AppBar';
+import Button from '../components/Button';
+
+const LogInScreen = ()=> {
+  return(
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="password" />
+        <Button label='Submit' />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default LogInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E5E5E5',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    backgroundColor: '#fff',
+    fontSize: 16,
+    // lineHeight: 32,
+    height: 48,
+    paddingHorizontal: 8,
+    opacity: 0.5,
+    borderColor: '#DDD',
+    borderWidth: 1,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#467FD3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#fff',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
+  }
+});
