25d915f75ebe5ca9367b3c5414d695cb025c611e
Kenta Shinozaki <kurometaro331@gmail.com>
2021-07-24 12:01:38 +0900

    Memo作成、編集画面にキーボード表示

---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  View, Text, TextInput, StyleSheet, KeyboardAvoidingView
+} from 'react-native';
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+const MemoEditScreen = ()=> {
+  return(
+    <KeyboardAvoidingView style={styles.container} behavior="height">
+      <AppBar />
+      <View style={styles.inputContainer}>
+        <TextInput value="買い物リスト" multiline style={styles.input}/>
+      </View>
+      <CircleButton name="check" />
+    </KeyboardAvoidingView>
+  );
+}
+
+export default MemoEditScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E5E5E5'
+  },
+  inputContainer: {
+    flex: 1,
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  }
+});
